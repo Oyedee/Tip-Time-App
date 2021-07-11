fe0cbed09945674f10748f6437ac9a23178efec5
Oyedee <oyempemia@gmail.com>
2021-07-11 18:11:15 +0100

    Enhancing user experience - hide soft keyboard and a few changes to activity_main.xml layout using scrollview

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.tiptime
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -14,7 +18,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         //calculate the tip
-        binding.calculateButton.setOnClickListener{calculateTip()}
+        binding.calculateButton.setOnClickListener {calculateTip()}
+        //Hide keyboard when user presses enter key
+        binding.costOfServiceEditText.setOnKeyListener {view, keyCode, _ -> handleKeyEvent(view, keyCode)}
+    }
+
+    //Hide Keyboard when Enter key is pressed
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 
     private fun calculateTip() {
